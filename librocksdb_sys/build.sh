9d9efd30235d702684e598e2f04bd9bb82bb55c8
siddontang <siddontang@gmail.com>
2017-02-23 15:06:11 +0800

    support rocksdb 5.1.2 (#17)

--- librocksdb_sys/build.sh
+++ librocksdb_sys/build.sh
@@ -106,11 +106,11 @@ function compile_rocksdb() {
         return
     fi
 
-    version=v5.0.1
-    vernum=5.0.1
+    version=v5.1.2
+    vernum=5.1.2
     echo building rocksdb-$version
     rm -rf rocksdb-$vernum
-    download https://github.com/facebook/rocksdb/archive/$version.tar.gz rocksdb-$version.tar.gz e5443504b430c71c04f19f13ebc89a4b
+    download https://github.com/facebook/rocksdb/archive/$version.tar.gz rocksdb-$version.tar.gz b682f574363edfea0e2f7dbf01fc0e5b
     tar xf rocksdb-$version.tar.gz
     wd=`pwd`
     cd rocksdb-$vernum
