6ed9bb1f06cb8f877dbd56005e2ac781b5318a49
Nitin Gupta <nitingupta910@gmail.com>
2017-02-26 19:39:22 -0800

    Update to rocksdb v5.1.4
    Existing version throws error when open a
    db or creating a column family with option:
      optimize_for_point_lookup
    
    Also some fixes around building rocksdb.

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:f201a44b4102308b840b15d9b89122af787476f1";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2016-10-27";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:462c21dda61aed0d5cb12283e8f02b757a7effdf";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-02-26";
 const char* rocksdb_build_compile_date = __DATE__;
