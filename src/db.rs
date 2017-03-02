f8d8f74bdfad8a00ca13be81d77c212523182d69
Nitin Gupta <nitingupta910@gmail.com>
2017-03-01 21:30:02 -0800

    impl Sync for ColumnFamily

--- src/db.rs
+++ src/db.rs
@@ -36,6 +36,8 @@ pub fn new_bloom_filter(bits: c_int) -> *mut ffi::rocksdb_filterpolicy_t {
 unsafe impl Send for DB {}
 unsafe impl Sync for DB {}
 
+unsafe impl Sync for ColumnFamily {}
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum DBCompressionType {
     None = ffi::rocksdb_no_compression as isize,
