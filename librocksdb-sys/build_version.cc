7ac66dafc5df2533d640187076028e99152ae956
Nitin Gupta <nitingupta910@gmail.com>
2017-05-01 09:13:08 -0700

    Update to rocksdb v5.2.1

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:462c21dda61aed0d5cb12283e8f02b757a7effdf";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-02-26";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:e52d6ad227d5fc2aecb30fada8979df2b47ee0db";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-05-01";
 const char* rocksdb_build_compile_date = __DATE__;
