3935cd072229c72ef651ffaa60243335b1a58f00
André Silva <andre.beat@gmail.com>
2018-08-31 10:44:17 +0100

    set cmake system name for android cross-compilation builds

--- rocksdb-sys/build.rs
+++ rocksdb-sys/build.rs
@@ -13,6 +13,15 @@ fn main() {
 
     let snappy = env::var_os("DEP_SNAPPY_INCLUDE").expect("DEP_SNAPPY_INCLUDE is set in snappy.");
 
+    // NOTE: the cfg! macro doesn't work when cross-compiling, it would return values for the host
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_android = target_os.contains("android");
+
+    if target_android {
+        // when cross-compiling CMAKE_SYSTEM_NAME is set to the host OS
+        cfg.define("CMAKE_SYSTEM_NAME", "Android");
+    }
+
     if cfg!(target_env = "msvc") {
         cfg.env("SNAPPY_INCLUDE", snappy);
 
@@ -28,7 +37,6 @@ fn main() {
             .define("SNAPPY_LIBRARIES", "/dev/null");
     }
 
-    // NOTE: the cfg! macro doesn't work when cross-compiling, it would return values for the host
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     if target_arch == "arm" || target_arch == "aarch64" {
         cfg.define("PORTABLE", "ON");
@@ -53,7 +61,7 @@ fn main() {
     // https://github.com/alexcrichton/cc-rs/blob/ca70fd32c10f8cea805700e944f3a8d1f97d96d4/src/lib.rs#L891
     if cfg!(any(target_os = "macos", target_os = "freebsd", target_os = "openbsd")) {
         println!("cargo:rustc-link-lib=c++");
-	} else if cfg!(not(any(target_env = "msvc", target_os = "android"))) {
+	} else if cfg!(not(target_env = "msvc")) && !target_android {
         println!("cargo:rustc-link-lib=stdc++");
     }
 }
