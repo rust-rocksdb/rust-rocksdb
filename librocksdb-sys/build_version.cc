0e19bfbee8cd343f286883315daa1c1222b0959d
Bushido Cabellero <lispy@curvybits.org>
2019-06-14 21:59:36 -0500

    Bumps to RocksDB version 6.1.2

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:641fae60f63619ed5d0c9d9e4c4ea5a0ffa3e253";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-02-11";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:2b38e2dd6602a17a2010308580fd5d8c91dea650";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2019-06-04";
 const char* rocksdb_build_compile_date = __DATE__;
