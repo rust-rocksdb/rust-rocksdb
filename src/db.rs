932a70e1f170e6bb7c76dc6e241920e021600229
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-09-07 16:26:40 +0300

    Add `DB::latest_sequence_number` method. (#326)

--- src/db.rs
+++ src/db.rs
@@ -1560,6 +1560,11 @@ impl DB {
             Err(e) => Err(e),
         }
     }
+
+    /// The sequence number of the most recent transaction.
+    pub fn latest_sequence_number(&self) -> u64 {
+        unsafe { ffi::rocksdb_get_latest_sequence_number(self.inner) }
+    }
 }
 
 impl WriteBatch {
