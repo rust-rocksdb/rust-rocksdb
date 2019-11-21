abec76f2759b6eb7633e51173300592887bb4a42
Bernardo Meurer <meurerbernardo@gmail.com>
2019-11-20 23:40:39 -0800

    librocksdb-sys: nuke fail_on_empty_directory

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -4,17 +4,6 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
-fn fail_on_empty_directory(name: &str) {
-    if fs::read_dir(name).unwrap().count() == 0 {
-        println!(
-            "The `{}` directory is empty, did you forget to pull the submodules?",
-            name
-        );
-        println!("Try `git submodule update --init --recursive`");
-        panic!();
-    }
-}
-
 fn bindgen_rocksdb() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
