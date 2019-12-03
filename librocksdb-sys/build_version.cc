a2c5d53231c9bc566e7164bfc4ce321a23a5d370
Florentin Dubois <florentin.dubois@corp.ovh.com>
2019-12-03 23:37:17 +0100

    feat(rocksdb): upgrade rocksdb to v6.4.6
    * Update Cargo.toml to match rocksdb version

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@76a56d89a7740f8dbb01edabf1ea5abc95a67657@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/10/16 09:47:38@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@e3169e3ea8762d2f34880742106858a23c8dc8b7@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2019/10/21 12:07:58@";
 const char* rocksdb_build_compile_date = __DATE__;
