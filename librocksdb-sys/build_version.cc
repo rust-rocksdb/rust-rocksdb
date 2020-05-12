70aa3914c30de6fddd3d9f4c1786f0e5ad10b048
Andronik Ordian <write@reusable.software>
2020-05-12 12:46:11 +0200

    librocksdb-sys: update rocksdb to 6.8.1 (#428)

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@0915c99f01b46f50af8e02da8b6528156f584b7c@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/03/18 18:51:16@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@27e593fbe10efa5a562ca79b1939ffb69d25163f@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/04/16 15:32:34@";
 const char* rocksdb_build_compile_date = __DATE__;
