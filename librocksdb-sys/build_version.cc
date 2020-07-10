d6c779c179aa3e5f68e4d6a3dfef27a6f776d92c
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2020-07-10 13:43:27 +0300

    Bump rocksdb up to 6.10.2 (#444)

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@27e593fbe10efa5a562ca79b1939ffb69d25163f@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/04/16 15:32:34@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@1482c8697c260cc12f7fce9f15401a85330d2ff1@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/05/23 13:48:40@";
 const char* rocksdb_build_compile_date = __DATE__;
