1b89e876c552479ef073f9d02259558ead8af0e3
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2020-10-13 20:49:12 +0300

    Update rocksdb revision

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@f548a2a03cacdf1463bb4c63340569f846c73128@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/10/12 11:20:45@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@878b3a41de96aefd67e9d94860b60a1bedf6320f@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/10/12 12:44:33@";
 const char* rocksdb_build_compile_date = __DATE__;
