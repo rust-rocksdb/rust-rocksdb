775b1241996063cc88338429ee0f51ee6af5a354
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2020-11-20 14:29:35 +0200

    Try to use rocksdb 6.11.4 version

--- librocksdb-sys/build_version.cc
+++ librocksdb-sys/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@97bf78721b7d9c1fa25e6a9b38b693d45e85196d@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/10/13 17:46:31@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@48bfca38f6f175435052a59791922a1a453d9609@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/07/15 17:54:15@";
 const char* rocksdb_build_compile_date = __DATE__;
