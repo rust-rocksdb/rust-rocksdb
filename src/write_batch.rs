68cb6f11640e064cdbe60506c56e31cf0e85c3c4
Yiyuan Liu <yiyuanliu1997@gmail.com>
2021-11-03 16:33:48 +0800

    Update doc for WriteBatch

--- src/write_batch.rs
+++ src/write_batch.rs
@@ -224,6 +224,8 @@ impl WriteBatch {
     /// Removes the database entries in the range ["begin_key", "end_key"), i.e.,
     /// including "begin_key" and excluding "end_key". It is not an error if no
     /// keys exist in the range ["begin_key", "end_key").
+    ///
+    /// `TxnDB` doesn't support `delete_range`.
     pub fn delete_range<K: AsRef<[u8]>>(&mut self, from: K, to: K) {
         let (start_key, end_key) = (from.as_ref(), to.as_ref());
 
@@ -243,6 +245,8 @@ impl WriteBatch {
     /// Removes the database entries in the range ["begin_key", "end_key"), i.e.,
     /// including "begin_key" and excluding "end_key". It is not an error if no
     /// keys exist in the range ["begin_key", "end_key").
+    ///
+    /// `TxnDB` doesn't support `delete_range_cf`.
     pub fn delete_range_cf<K: AsRef<[u8]>>(&mut self, cf: &impl AsColumnFamilyRef, from: K, to: K) {
         let (start_key, end_key) = (from.as_ref(), to.as_ref());
 
