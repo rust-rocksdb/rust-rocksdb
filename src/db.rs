dcb9cde28b47eccd76faaa79ff6a367ce66afa87
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2022-01-11 10:01:13 +0200

    Apply redundant_closure_for_method_calls suggestion by clippy

--- src/db.rs
+++ src/db.rs
@@ -1330,9 +1330,9 @@ impl<T: ThreadMode> DBWithThreadMode<T> {
             ffi::rocksdb_compact_range(
                 self.inner,
                 opt_bytes_to_ptr(start),
-                start.map_or(0, |s| s.len()) as size_t,
+                start.map_or(0, <[u8]>::len) as size_t,
                 opt_bytes_to_ptr(end),
-                end.map_or(0, |e| e.len()) as size_t,
+                end.map_or(0, <[u8]>::len) as size_t,
             );
         }
     }
@@ -1352,9 +1352,9 @@ impl<T: ThreadMode> DBWithThreadMode<T> {
                 self.inner,
                 opts.inner,
                 opt_bytes_to_ptr(start),
-                start.map_or(0, |s| s.len()) as size_t,
+                start.map_or(0, <[u8]>::len) as size_t,
                 opt_bytes_to_ptr(end),
-                end.map_or(0, |e| e.len()) as size_t,
+                end.map_or(0, <[u8]>::len) as size_t,
             );
         }
     }
@@ -1375,9 +1375,9 @@ impl<T: ThreadMode> DBWithThreadMode<T> {
                 self.inner,
                 cf.inner(),
                 opt_bytes_to_ptr(start),
-                start.map_or(0, |s| s.len()) as size_t,
+                start.map_or(0, <[u8]>::len) as size_t,
                 opt_bytes_to_ptr(end),
-                end.map_or(0, |e| e.len()) as size_t,
+                end.map_or(0, <[u8]>::len) as size_t,
             );
         }
     }
@@ -1399,9 +1399,9 @@ impl<T: ThreadMode> DBWithThreadMode<T> {
                 cf.inner(),
                 opts.inner,
                 opt_bytes_to_ptr(start),
-                start.map_or(0, |s| s.len()) as size_t,
+                start.map_or(0, <[u8]>::len) as size_t,
                 opt_bytes_to_ptr(end),
-                end.map_or(0, |e| e.len()) as size_t,
+                end.map_or(0, <[u8]>::len) as size_t,
             );
         }
     }
