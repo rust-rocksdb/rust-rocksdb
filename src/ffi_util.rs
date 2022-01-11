a2e5ebd94104e596355f80487593ffd5ace1f49b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-01-11 20:51:41 +0100

    Optimize non-overlapping copy in raw_data

--- src/ffi_util.rs
+++ src/ffi_util.rs
@@ -29,8 +29,8 @@ pub(crate) unsafe fn raw_data(ptr: *const c_char, size: usize) -> Option<Vec<u8>
         None
     } else {
         let mut dst = Vec::with_capacity(size);
+        ptr::copy_nonoverlapping(ptr as *const u8, dst.as_mut_ptr(), size);
         dst.set_len(size);
-        ptr::copy(ptr as *const u8, dst.as_mut_ptr(), size);
         Some(dst)
     }
 }
