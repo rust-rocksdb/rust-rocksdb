b1e9ce64e932c0c716acf024767626b1222b3645
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-02-26 12:44:20 +0100

    Add ReadOptions::set_async_io()

--- src/db_options.rs
+++ src/db_options.rs
@@ -3440,6 +3440,12 @@ impl ReadOptions {
             ffi::rocksdb_readoptions_set_pin_data(self.inner, c_uchar::from(v));
         }
     }
+
+    pub fn set_async_io(&mut self, v: bool) {
+        unsafe {
+            ffi::rocksdb_readoptions_set_async_io(self.inner, c_uchar::from(v));
+        }
+    }
 }
 
 impl Default for ReadOptions {
