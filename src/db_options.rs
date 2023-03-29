1d61bae77773a8769f94be00525363a98ddc9146
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-03-29 17:55:21 +0200

    Document set_async_io()

--- src/db_options.rs
+++ src/db_options.rs
@@ -3441,6 +3441,11 @@ impl ReadOptions {
         }
     }
 
+    /// Asynchronously prefetch some data.
+    ///
+    /// Used for sequential reads and internal automatic prefetching.
+    ///
+    /// Default: `false`
     pub fn set_async_io(&mut self, v: bool) {
         unsafe {
             ffi::rocksdb_readoptions_set_async_io(self.inner, c_uchar::from(v));
