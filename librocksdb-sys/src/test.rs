c1fbc00d6911f38d6422dad388184ac9240cd4ec
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-12-23 15:50:27 +0000

    chore: fix internal test

--- librocksdb-sys/src/test.rs
+++ librocksdb-sys/src/test.rs
@@ -39,7 +39,7 @@ fn internal() {
         rocksdb_options_optimize_level_style_compaction(opts, 0);
         rocksdb_options_set_create_if_missing(opts, u8::from(true));
 
-        let rustpath = "_rust_rocksdb_internaltest";
+        let rustpath = "/tmp/_rust_rocksdb_internaltest";
         let cpath = CString::new(rustpath).unwrap();
 
         let mut err: *mut c_char = ptr::null_mut();
